10a498c202ce5d8211b9ca722f09b76162258cc9
Brian Ramirez <bramirez@gse.harvard.edu>
2019-12-04 18:30:29 -0500

    added Brian's changes and experience

---
+++ Assets/Scrips/Brian/BoardGameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class BoardGameManager : MonoBehaviour{
+
+    Dictionary<string,bool> boxes = new Dictionary<string,bool>();
+    void Start(){
+        boxes.Add("box1", false);
+        boxes.Add("box2", false);
+        boxes.Add("box3", false);
+        boxes.Add("box4", false);
+        boxes.Add("box5", false);
+        boxes.Add("box6", false);
+    }
+
+    void Update(){
+        if(isGameOver()){
+            TextMeshPro monitor = GameObject.Find("MonitorText").GetComponent<TextMeshPro>();
+            monitor.SetText("Congratulations! You solved the puzzle\n\nTap the A button to go to the next experience.");
+            if(OVRInput.GetDown(OVRInput.Button.One)){
+                // Change Scene Name Here
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+    public void changeBoxStatus(string boxName, bool value){
+        boxes[boxName] = value;
+    }
+
+    bool isGameOver(){
+        foreach(KeyValuePair<string, bool> box in boxes){
+            if(box.Value == false){
+                return false;
+            }
+        }
+        return true;
+    }
+}
